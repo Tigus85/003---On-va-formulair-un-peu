352724ac98fc5bb57002500f6643684d44e8953e
Nicolas SWIGON <swigon.nicolas@gmail.com>
2022-04-29 09:42:36 +0200

    fin 1er et debut 2nd

---
+++ Easy avec une petite form/destination.php
@@ -0,0 +1,50 @@
+<?php
+
+var_dump((strlen($_POST["pass"]) >=6));
+var_dump($_POST['pass'] === $_POST['confirm_pass']);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Destination</title>
+</head>
+<body>
+
+<?php
+
+var_dump($_POST);
+
+if (
+  !empty($_POST['name'])
+  && !empty($_POST['firstName'])
+  && !empty($_POST['pass'])
+  && !empty($_POST['confirm_pass'])
+  && !empty($_POST['email'])
+  && $_POST['pass'] === $_POST['confirm_pass']
+  && filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)
+  && (strlen($_POST["pass"]) >=6)
+) { ?>
+
+  <div class="destination">
+  <span> <?= $_POST['name']?> <?= $_POST['firstName']?></span>   s'est inscrit correctement avec le mot de passe valide qui est <span> <?= $_POST['pass']?> </span> , et avec son adresse email suivante : <span> <?= $_POST['email']?> </span> 
+  </div>
+
+<?php
+} else { 
+
+  echo "<h2>Erreur au niveau de la saisie</h2>";
+
+
+}
+?>
+ 
+</body>
+</html>
